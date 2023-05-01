0221b4cedfcddf50ca9ae03d80baaa821e6b3e74
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-01 14:07:35 +0530

    server running

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const router = require("./Router/router");
 const cors = require("cors");
 const errorHandler = require("./Middleware/middleware");
 const app = express();
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
