b7cb5e4b20795094e15e7b149150195821f470ca
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-01 13:27:42 +0530

    build project

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const router = require("./Router/router");
 const cors = require("cors");
 const errorHandler = require("./Middleware/middleware");
 const app = express();
-const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 200,
-};
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use("/api", router);
 app.use(errorHandler);
 app.listen(5500, () => {
