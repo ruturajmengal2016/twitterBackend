9e8fca6a6e27acc52789982d3aa44042032ae12e
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-02 20:55:04 +0530

    build

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const errorHandler = require("./Middleware/middleware");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["PUT", "POST", "GET", "DELETE"],
+    Credential: true,
+  })
+);
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,,POST,PUT");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 app.use("/api", router);
 app.use(errorHandler);
 app.listen(5500, () => {
